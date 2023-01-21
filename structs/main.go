d3add5e004168e2d03dd749b8f3780abbd3b92ce
phaneendrababu <phaneendrababuthota@gmail.com>
2023-01-22 02:53:11 +0530

    struct crreation

---
+++ structs/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+	/*alex := person{firstName: "Alex", lastName: "Anderson"}
+	fmt.Println(alex)*/
+	var alex person
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
+}
